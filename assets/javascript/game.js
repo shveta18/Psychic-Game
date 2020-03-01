7911c7f1deecd2d8463064de7c7b522d6d216467
Shveta Kumar <sk@shveta.ca>
2020-03-01 16:19:47 -0500

    updated code to account for non alphabet keys entered and to display resets correctly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,14 +7,18 @@ var letterChoices = ["a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m",
         //user gudesses to be stored in an array to be displayed later
         var userGuess = [];
 
+         //determines which letter the computer chose
+        var compPick = letterChoices[Math.floor(Math.random() * letterChoices.length)];
+        console.log("Computer chose" + " " + compPick);
+
+
         document.onkeyup = function (event) {
-            //As long as the user has 9 tries left, continue to push the values the user entered into the array
-            if (guessesLeft > 0) {
-                //determines which letter the computer chose
-                var compPick = letterChoices[Math.floor(Math.random() * letterChoices.length)];
-                console.log("Computer chose" + " " + compPick);
-                var userPick = event.key
-                console.log("User Guess is" + " " + userPick);
+
+            var userPick = event.key;
+            console.log("User Guess is" + " " + userPick);
+            
+            //If the user inputs a value from the array, the game registers the keystroke.
+            if (letterChoices.includes(userPick)) { 
 
                 // within this if statement if the user pick matches computer pick, add 1 to the win, and reset the guesses left back to 9
                 if (userPick === compPick) {
@@ -26,6 +30,13 @@ var letterChoices = ["a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m",
                     console.log("Emptied the array to start over");
                     //display the total wins without resetting
                     document.getElementById("wins").innerHTML = wins;
+                    //display the total guesses reset to 9
+                    document.getElementById("guesses-left").innerHTML = guessesLeft;
+                    //resetting what the computer chose to another value
+                    compPick = letterChoices[Math.floor(Math.random() * letterChoices.length)];
+                    console.log("Computer chose NEW value" + " " + compPick);
+                    //display the now empty array
+                    document.getElementById("Guesses").innerHTML = userGuess;
 
                 }
                 //if the user pick doesn't match computer entry.  
@@ -41,13 +52,20 @@ var letterChoices = ["a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m",
                     console.log("the guesses left are: " + guessesLeft);
                     // the code below is for when the user has reached the max tries, the game should reset. 
                     if (guessesLeft === 0) {
-                        console.log("does ths work?");
+                        console.log("no more guesses left");
                         userGuess.length = 0;
-                        console.log("reset the user key array");
+                        console.log("reset the userGuess array to empty");
                         losses++;
                         document.getElementById("losses").innerHTML = losses;
                         guessesLeft = 9;
+                        //display the total guesses reset to 9
+                        document.getElementById("guesses-left").innerHTML = guessesLeft;
                         console.log("guesses have been reset after 9 tries. Game begins again");
+                        //display the now empty array
+                        document.getElementById("Guesses").innerHTML = userGuess;
+                        //resetting what the computer chose to another value
+                        compPick = letterChoices[Math.floor(Math.random() * letterChoices.length)];
+                        console.log("Computer chose NEW value" + " " + compPick);
                     };
                 };
 
